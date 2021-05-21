ed293833530057a228ebf40afbf964ce7caee404
p00klap <84356020+p00klap@users.noreply.github.com>
2021-05-21 10:49:10 +0300

    Update hw4.cpp

--- hw4.cpp
+++ hw4.cpp
@@ -1,4 +1,4 @@
-#include <iostream>;
+#include <iostream>
  int main (){
      int arr[3][3][3];
      arr [1][1][1] = 100;
@@ -6,4 +6,6 @@
      int(*a)[3][3] = arr;
      
      std::cout << *(*(*(a+1)+1)+1) << std::endl;
+  
+  return 0;
  }
